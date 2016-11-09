f52a2d0a52c3701d5efd29ba0e9642e9c40d5b6a
LILouis <lijunpeng91@gmail.com>
2016-11-09 13:14:39 -0500

    init project

---
+++ MineSweeper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MineSweeper
+//
+//  Created by LILouis on 11/9/16.
+//  Copyright © 2016 LILouis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MineSweeper/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MineSweeper
+//
+//  Created by LILouis on 11/9/16.
+//  Copyright © 2016 LILouis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
